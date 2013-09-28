a64cede0cc2f3356bada0f39462f7716fd95edf3
sky <sky@a70f5666-fd2b-40d0-8f41-e30882f606c2>
2013-09-28 22:40:10 +0000

    And it finally works.
    git-svn-id: https://svn.q1cc.net/repository/sky/_tool@912 a70f5666-fd2b-40d0-8f41-e30882f606c2

--- tool.sh
+++ tool.sh
@@ -2,4 +2,7 @@
 
 echo "Sorry, for now you will have to do it manually."
 echo "But this directory still contains everything you need."
+echo ""
+echo "A good way to start is to use all the settings..."
+echo "    ln -s _tool/homedir/.[^.]* ."
 
