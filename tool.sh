5602e29078a11c8487ce5ccc62d9f00abd2c9f3b
Markus Dangl <markus@q1cc.net>
2014-01-13 07:09:16 +0100

    Don't forget to install ~/bin

--- tool.sh
+++ tool.sh
@@ -5,4 +5,7 @@ echo "But this directory still contains everything you need."
 echo ""
 echo "A good way to start is to use all the settings..."
 echo "    ln -s _dotfiles/*/.* ."
+echo ""
+echo "And maybe the launcher / tool scripts..."
+echo "    cd bin; cp -s ~/_dotfiles/bin/* ."
 
