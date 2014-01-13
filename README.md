2cdabda139224e9d10d5d70006c3fc1d34638ae5
Markus Dangl <markus@q1cc.net>
2014-01-13 06:43:41 +0100

    Fixed paths (it's called dotfiles now).

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ dotfiles
 ========
 
 Personal settings and stuff
+
+Notes
+=====
+
+The misc dir is important (it hooks in some things like the myrc scripts...)
+I am too lazy to write a installer shell script, since i don't have to to it a lot.
+
--- tool.sh
+++ tool.sh
@@ -4,5 +4,5 @@ echo "Sorry, for now you will have to do it manually."
 echo "But this directory still contains everything you need."
 echo ""
 echo "A good way to start is to use all the settings..."
-echo "    ln -s _tool/homedir/.[^.]* ."
+echo "    ln -s _dotfiles/*/.* ."
 
