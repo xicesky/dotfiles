3d5b456c84b50ddca8d60510d9f6bd05218fb919
Markus Dangl <markus@q1cc.net>
2015-02-11 12:49:14 +0100

    TODO, removed comments.

--- scripts/in-browser/deutschlandsim-rechnungen.js
+++ scripts/in-browser/deutschlandsim-rechnungen.js
@@ -3,6 +3,10 @@
     Author: Markus Dangl
     License: BSD
     !No warranties of any kind!
+
+    TODO:
+        - Find a hack to get the _SID cookie (it's HttpOnly...)
+
 */
 
 // Array to put the extracted data in
@@ -79,20 +83,6 @@ out_pre.append('# Fetch the real cookie from the network page in the developer t
 out_pre.append('# and save it as COOKIE environment variable').append('<br/>');
 out_pre.append('COOKIE=""').append('<br/>');
 data.forEach(function(d) {
-    /* CURL:
-        curl "https://service.deutschlandsim.de/mytariff/invoice/showPDF/180816885"
-        -H "Pragma: no-cache"
-        -H "Cache-Control: no-cache"
-        -H "Referer: https://service.deutschlandsim.de/mytariff/invoice/show"
-        -H "Cookie: _ga=GA1.2.749832728.1423602770; isCookieAllowed=true; _SID=salr01qvm9f0einv5avb20gl86; sw_UNC=MDAwMjY0NjU3NTc079T"%"2FVMJWnVM"%"3D; __utma=177079856.749832728.1423602770.1423611747.1423651526.4; __utmb=177079856.8.10.1423651526; __utmc=177079856; __utmz=177079856.1423602770.1.1.utmgclid=Cj0KEQiA9eamBRDqvIz_qPbVteABEiQAnIBTEEMLCdyjHbYvRWByKDfowgJ5A-DhrY15ncBeoMD4dVYaAsbu8P8HAQ|utmccn=(not"%"20set)|utmcmd=(not"%"20set)|utmctr=(not"%"20provided)"
-    */
-    // -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
-    // -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36"
-    // -H "Accept-Encoding: gzip, deflate, sdch"
-    // -H "Accept-Language: de-DE,de;q=0.8,en-GB;q=0.6,en;q=0.4,en-US;q=0.2" 
-    // -H "Connection: keep-alive"
-    // --compressed
-
     var fn = d.title.replace(/[^a-zA-Z0-9]/g, '_') + '.pdf';
     var p = out_pre;
     p.append('curl "' + d.url + '" -H "Cookie: $COOKIE" -o "' + fn + '"').append('<br/>');
