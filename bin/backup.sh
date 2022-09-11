35d58399098bc2697fca516498f00a1c543ed7b1
Markus Dangl <663399+xicesky@users.noreply.github.com>
2022-09-11 15:15:12 +0200

    backup.sh: argument parsing

--- bin/backup.sh
+++ bin/backup.sh
@@ -2,6 +2,7 @@
 
 VERBOSITY="${VERBOSITY:-2}"
 NO_ACT="${NO_ACT:-false}"
+REMOVE=false
 
 # Display a message if verbosity level allows
 log() {
@@ -11,6 +12,14 @@ log() {
     if [[ "$VERBOSITY" -ge "$level" ]] ; then
         echo "$prefix$*" 1>&2
     fi
+}
+
+# Same but use printf
+logf() {
+    declare level="$1"; shift
+    # Passing a formatting string is exactly what we intend here
+    # shellcheck disable=SC2059
+    log "$level" "$(printf "$@")"
 }
 
 invoke() {
@@ -32,15 +41,95 @@ report_command_failure() {
     fi
 }
 
-main() {
-    SRC="$1"; shift
-    BUPDIR="${BUPDIR:-/e/Backup/2022-09-10-Backup-MobileX-Notebook}"
-    target="$(basename "$SRC").7z"
-    targetdir="$(dirname "$SRC")"
+canonical-path() {
+    if [[ -d "$1" ]] ; then
+        # If it's a directory, just cd and pwd
+        ( cd "$1" && pwd; )
+    else
+        # For files just canonicalize the dirname
+        echo "$(canonical-path "$(dirname "$1")")/$(basename "$1")"
+    fi
+}
 
-    invoke mkdir -p "$BUPDIR/$targetdir" || return 1
-    invoke 7z a "$BUPDIR/$targetdir/$target" "$SRC" || return 1
-    invoke rm -rf "$SRC"
+backup() {
+    src="$(canonical-path "$1")"; shift
+    [[ "$src" != /* ]] && { echo "Error: source path must be absolute: $src" 1>&2; return 1; }
+
+    target="$(basename "$src").7z"
+    targetdir="$BUPDIR/$(dirname "$src")"
+    is_archive=false
+    if [[ "$src" = *.7z || "$src" = *.zip || "$src" = *.tar.* ]] ; then
+        is_archive=true
+        target="$(basename "$src")"
+    fi
+
+    invoke mkdir -p "$targetdir" || return 1
+    if $is_archive ; then
+        invoke cp "$src" "$targetdir/$target" || return 1
+    else
+        invoke 7z a "$targetdir/$target" "$src" || return 1
+    fi
+    if $REMOVE ; then
+        invoke rm -rf "$src" || return 1
+    fi
+}
+
+help() {
+    log 0 ""
+    log 0 "$THIS_SCRIPT_NAME - simple backup script v$THIS_SCRIPT_VERSION"
+    log 0 ""
+    log 0 "Usage:"
+    logf 0 "    %s [options] <file>\n" "$THIS_SCRIPT_NAME"
+    log 0 ""
+}
+
+main() {
+    BUPDIR="${BUPDIR:-/e/Backup/2022-09-10-Backup-MobileX-Notebook}"
+    declare -a sources=()
+
+        # Parse arguments
+    while [[ $# -gt 0 ]] ; do
+        arg="$1"; shift
+        case "$arg" in
+        --help|help)
+            help "$@"
+            return 0
+            ;;
+        --version|version)
+            # Display version and exit
+            echo "$THIS_SCRIPT_VERSION"
+            return 0
+            ;;
+        -v|--verbose)
+            (( VERBOSITY++ ))
+            ;;
+        -q|--quiet)
+            (( VERBOSITY-- ))
+            ;;
+        -x|--act)
+            NO_ACT=false
+            ;;
+        -n|--no-act)
+            NO_ACT=true
+            ;;
+        --rm)
+            REMOVE=true
+            ;;
+        -*)
+            error 1 "Unknown option: $arg" || true
+            echo "    ... here's the help:" 1>&2
+            help
+            return 1
+            ;;
+        *)
+            sources+=( "$arg" )
+            ;;
+        esac
+    done
+
+    for i in "${sources[@]}" ; do
+        backup "$i" || return $?
+    done
 }
 
 main "$@" || {
