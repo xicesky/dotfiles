d95ca09a03f0671d213aa3fb76d977c1dc243eab
Markus Dangl <663399+xicesky@users.noreply.github.com>
2023-01-02 23:49:43 +0100

    kube-helper now for various cfgs

--- bin/kube-helper.sh
+++ bin/kube-helper.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+KUBE_CONFIG_FILE="${KUBE_CONFIG_FILE:-config}"
+KUBE_NAMESPACE="${KUBE_NAMESPACE:-default}"
 SPCUSTOMER="${SPCUSTOMER:-"customer-687399035"}"
 
-remap-sp-customer() {
+config-for-sp() {
+    KUBE_CONFIG_FILE="config-az-mx-dev.yaml"
+    SPCUSTOMER="$1"
+    KUBE_NAMESPACE="$SPCUSTOMER"
+}
+
+config-for-local-k3d() {
+    KUBE_CONFIG_FILE="config-local-k3d-default.yaml"
+    SPCUSTOMER=""
+    KUBE_NAMESPACE="default"
+}
+
+load-config() {
     case "$1" in
-    harg*)      echo "customer-687399035" ;;
-    ochs*)      echo "customer-687399036" ;;
-    customer-*) echo "$1" ;;
+    harg*)      config-for-sp "customer-687399035" ;;
+    ochs*)      config-for-sp "customer-687399036" ;;
+    customer-*) config-for-sp "$1" ;;
+    local*)     config-for-local-k3d "$1" ;;
     *)          return 1 ;;
     esac
 }
@@ -58,36 +73,49 @@ ship-bash-function() {
 }
 
 # kubectl alias with namespace
-kube() { kubectl -n "$SPCUSTOMER" "$@"; }
+kube() { kubectl -n "$KUBE_NAMESPACE" "$@"; }
+
+kmipexec_usage() {
+    echo "Usage: kmipexec [-p <podname>|--pod <podname>] <command ...>"
+    echo "    e.g.: kmipexec -p mipserver-customer-687399036-0 bash"
+    [[ -n "$1" ]] && echo "    podname defaults to: $pod"
+}
 
 # execute command on mipserver pod
 kmipexec() {
-    declare pod="mipserver-${SPCUSTOMER}-0"
+    declare pod=""
     [[ "$1" = --pod || "$1" == -p ]] && { shift; pod="$1"; shift; }
+    [[ -z "$pod" && -n "$SPCUSTOMER" ]] && pod="mipserver-${SPCUSTOMER}-0"
+    if [[ -z "$pod" ]] ; then
+        echo "No pod name provided and no serviceplatform customer (SPCUSTOMER variable) set." 1>&2
+        kmipexec_usage 1>&2
+        return 1
+    fi
     [[ "$#" -lt 1 ]] && {
-        echo "Usage: kmipexec [-p <podname>|--pod <podname>] <command ...>" 1>&2
-        echo "    e.g.: kmipexec -p mipserver-customer-687399036-0 bash" 1>&2
-        echo "    podname defaults to: $pod" 1>&2
+        kmipexec_usage "$pod" 1>&2
         return 1
     }
     MSYS2_ARG_CONV_EXCL="*" kube exec "$pod" -it -c dispatchx-mipserver -- "$@"; 
 }
 
 kmipdebug() {
-    declare pod="mipserver-${SPCUSTOMER}-0"
+    declare pod=""
     [[ "$1" = --pod || "$1" == -p ]] && { shift; pod="$1"; shift; }
+    [[ -z "$pod" && -n "$SPCUSTOMER" ]] && pod="mipserver-${SPCUSTOMER}-0"
     kube port-forward "$pod" 8787:8787
 }
 
 cmd_print() {
     # set KUBECONFIG
-    echo "export KUBECONFIG=~/.kube/az-mx-dev-kubeconfig.yaml"
+    echo "export KUBECONFIG=~/.kube/$KUBE_CONFIG_FILE"
+    echo "export KUBE_NAMESPACE=\"$KUBE_NAMESPACE\""
     echo "export SPCUSTOMER=\"$SPCUSTOMER\""
+
     ship-bash-function kube "kubctl alias with namespace"
+    ship-bash-function kmipexec_usage "usage for kmipexec"
     ship-bash-function kmipexec "execute command on mipserver pod"
     ship-bash-function kmipdebug "foward port 8787"
 }
-
 
 help() {
     echo "Sorry, help NYI"
@@ -96,6 +124,7 @@ help() {
 main() {
     declare command=print
     declare arg
+    declare target_config=""
 
     # Parse arguments
     declare argi=0
@@ -106,7 +135,7 @@ main() {
         *)
             case "$argi" in
             1)
-                SPCUSTOMER="$arg"
+                target_config="$arg"
                 ;;
             *)
                 echo "Error: unknown positional argument #$argi: $arg" 1>&2
@@ -119,12 +148,21 @@ main() {
         esac
     done
 
-    declare temp_customer
-    temp_customer="$(remap-sp-customer "${SPCUSTOMER}")" || {
-        echo "Unknown serviceplatform customer: $SPCUSTOMER" 1>&2
+    if [[ -z "$target_config" ]] ; then
+        {
+            echo "Available configs:"
+            echo "    customer-*      Serviceplatform customer namespace"
+            echo "    hargi           Shortcut for sp hargassner"
+            echo "    ochsi           Shortcut for sp ochsner"
+            echo "    local           Local k3d"
+        } 1>&2
+        return 0
+    fi
+
+    load-config "$target_config" || {
+        echo "Unknown config: $target_config" 1>&2
         return 1
     }
-    SPCUSTOMER="$temp_customer"
 
     declare exitcode=0
     "cmd_$command" "$@"
