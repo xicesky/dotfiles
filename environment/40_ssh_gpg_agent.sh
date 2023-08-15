96b0cb401aba501a821ed6d118090e30cd8c9a36
Markus Dangl <663399+xicesky@users.noreply.github.com>
2023-08-15 18:47:19 +0200

    env: Use new ssh-agent scripts, fixes in zsh README

--- environment/40_ssh_gpg_agent.sh
+++ environment/40_ssh_gpg_agent.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# FIXME: Should just be incorporated here directly
-if [[ -e "${HOME}/bin/my-ssh-pageant.sh" ]] ; then
+# FIXME: Should just be incorporated here directly if possible
+
+# OLD ssh-pageant behaviour - DEPRECATED
+# if [[ -e "${HOME}/bin/my-ssh-pageant.sh" ]] ; then
+#     # shellcheck disable=SC2016
+#     echo 'eval $(${HOME}/bin/my-ssh-pageant.sh)'
+# fi
+
+# NEW behaviour: Use eval script from _dotfiles/ssh/ssh-agent-scripts
+if [[ -e "${HOME}/bin/my-ssh-agent.eval.sh" ]] ; then
     # shellcheck disable=SC2016
-    echo 'eval $(${HOME}/bin/my-ssh-pageant.sh)'
+    echo 'eval $(${HOME}/bin/my-ssh-agent.eval.sh)'
 fi
 
 cat <<"EOF"
