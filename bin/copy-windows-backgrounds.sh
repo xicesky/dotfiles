62541e514641d6a75477de598595be2e57c16f0b
Markus Dangl <663399+xicesky@users.noreply.github.com>
2024-02-29 13:04:25 +0100

    Make copy-windows-backgrounds work on wsl.

--- bin/copy-windows-backgrounds.sh
+++ bin/copy-windows-backgrounds.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-lapdata="$(cygpath -u "$LOCALAPPDATA")"
+# Resolve windows LOCALAPPDATA path
+function resolve_local_app_data() {
+    if which cygpath &>/dev/null; then
+        # Cygwin & msys
+        cygpath -u "$LOCALAPPDATA"
+    elif which wslvar &>/dev/null; then
+        wslpath -u "$(wslvar LOCALAPPDATA)"
+    else
+        echo "Error: Could not resolve LOCALAPPDATA path" 1>&2
+        return 1
+    fi
+}
+
+lapdata="$(resolve_local_app_data)" || exit 1
+[[ -d "$lapdata" ]] || { echo "Error: Could not find LOCALAPPDATA directory" 1>&2; exit 1; }
+
 picturedir="$lapdata/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets"
 ff "$picturedir" -type f \
     | while IFS= read -r file; do
@@ -8,4 +23,3 @@ ff "$picturedir" -type f \
     [[ -e "$dest" ]] && continue
     cp -v "$file" "$dest"
 done
-
