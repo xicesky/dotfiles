70f04cd6a28817ed47569ba7744424a40ea15038
Markus Dangl <663399+xicesky@users.noreply.github.com>
2024-09-26 14:00:41 +0200

    bin/oauth2client.sh: Cleanup.

--- bin/oauth2client.sh
+++ bin/oauth2client.sh
@@ -5,14 +5,14 @@
 #   - ... and openssl for signing JWTs
 #   - jq for JSON parsing
 #   - curl for HTTP requests
-# Currently only supports the client credentials flow
+# Currently only supports the client credentials and device flow
 
 ################################################################################
 # Verbosity, command logging
 
 # Verbosity level, 0 is quiet, 1 is normal, 2 prints commands
 VERBOSITY="${VERBOSITY:-1}"
-DEBUG_TOKEN=true
+DEBUG_TOKEN="${DEBUG_TOKEN:-false}"
 
 # Don't act, just check (and print commands)
 NO_ACT="${NO_ACT:-false}"
@@ -125,7 +125,8 @@ which() {
 # TODO: Fetch info from .well-known/openid-configuration
 # curl "https://idp.kyma-dev.mobilex-serviceplatform.com/auth/realms/ProductDevelopment/.well-known/openid-configuration" | jq .
 
-CONFIG_MODE="${OAUTH_MODE:-oidc}" # Possible modes: oauth2, oidc, mx-configuration
+CONFIG_MODE="${OAUTH_MODE:-oidc}"                   # Possible modes: oauth2, oidc, mx-configuration
+DEFAULT_LOGIN_MODE="${DEFAULT_LOGIN_MODE:-device}"  # Possible modes: client-credentials, device
 
 # For mode "mx-configuration" only
 MX_CUSTOMER="${MX_CUSTOMER:-687399110}"
@@ -196,6 +197,9 @@ configure() {
             configure_via_mx_configuration
             return $?
             ;;
+        *)
+            echo "Error: Unknown CONFIG_MODE $CONFIG_MODE!" 1>&2
+            return 1
     esac
 }
 
@@ -217,6 +221,132 @@ is_token_expired() {
     #echo "expiry=$expiry"
     [ "$current" -ge "$expiry" ]
 }
+
+# Only used during login functions
+OAUTH2_RESPONSE_JSON=""
+
+login_via_client_credentials() {
+    declare access_token
+    if [[ -z "$OAUTH2_CLIENT_SECRET" ]] ; then
+        echo "Error: OAUTH2_CLIENT_SECRET is not set" 1>&2
+        return 1
+    fi
+
+    configure || return $?
+
+    # FIXME: Handle HTTP errors
+    OAUTH2_RESPONSE_JSON="$(
+        invoke curl -s -X POST \
+            --url "${OAUTH2_TOKEN_ENDPOINT}" \
+            --header 'Content-Type: application/x-www-form-urlencoded' \
+            --data 'grant_type=client_credentials' \
+            --data "client_id=$OAUTH2_CLIENT_ID" \
+            --data "client_secret=$OAUTH2_CLIENT_SECRET" \
+            --data "audience=$OAUTH2_AUDIENCE"
+    )"
+    OAUTH2_ACCESS_TOKEN="$(echo "$OAUTH2_RESPONSE_JSON" | jq -r '.access_token')"
+}
+
+login_via_device() {
+    # See https://git.mobilexag.de/prd-serviceplatform/documentation/-/blob/develop/modules/operations-guide/examples/read-token-from-keycloak.sh?ref_type=heads
+    # https://datatracker.ietf.org/doc/html/rfc8628#section-3.1
+    local device_code user_code verification_uri expires interval expire_seconds error access_token
+    local -a request_data
+
+    configure || return $?
+
+    request_data=(
+        --data "client_id=$OAUTH2_CLIENT_ID"
+        --data "scope=$OAUTH2_SCOPE"
+    )
+
+    if [ -n "$OAUTH2_CLIENT_SECRET" ] ; then
+        request_data+=( --data "client_secret=$OAUTH2_CLIENT_SECRET" )
+    fi
+    
+    OAUTH2_RESPONSE_JSON="$(
+        # Note: Client authentication is enabled in keycloak, so we need to pass the client_secret
+        # This is not neccessary for other clients like crossmip, where the client is public
+        invoke curl -s -X POST \
+            --url "$OAUTH2_DEVICE_AUTHORIZATION_ENDPOINT" \
+            --header 'content-type: application/x-www-form-urlencoded' \
+            ${request_data[@]}
+    )"
+    #echo "response_json: $OAUTH2_RESPONSE_JSON" 1>&2
+
+    error="$(echo "$OAUTH2_RESPONSE_JSON" | jq -r .error)"
+    if [ "$error" != "null" ]; then
+        echo "Error response from keycloak: $(printf "%q" "$error")" 1>&2
+        echo "$OAUTH2_RESPONSE_JSON" | jq . 1>&2
+        return 1
+    fi
+
+    device_code="$(echo "$OAUTH2_RESPONSE_JSON" | jq -r .device_code)"
+    user_code="$(echo "$OAUTH2_RESPONSE_JSON" | jq -r .user_code)"
+    verification_uri="$(echo "$OAUTH2_RESPONSE_JSON" | jq -r .verification_uri)"
+    expires="$(echo "$OAUTH2_RESPONSE_JSON" | jq -r .expires_in)"
+    interval="$(echo "$OAUTH2_RESPONSE_JSON" | jq -r .interval)"
+
+    (
+        echo "Please:"
+        echo "    * open $verification_uri"
+        echo "    * enter $user_code and log in"
+        open "$verification_uri"
+    ) 1>&2
+
+    # Loop until the token is available or the timeout is reached
+    expire_seconds=$((SECONDS+expires))
+    while [ $SECONDS -lt $expire_seconds ]; do
+        sleep "$interval"
+        OAUTH2_RESPONSE_JSON="$(
+            invoke curl -s -X POST \
+                --url "$OAUTH2_TOKEN_ENDPOINT" \
+                --header 'content-type: application/x-www-form-urlencoded' \
+                --data "client_id=$OAUTH2_CLIENT_ID" \
+                --data "client_secret=$OAUTH2_CLIENT_SECRET" \
+                --data "device_code=$device_code" \
+                --data "grant_type=urn:ietf:params:oauth:grant-type:device_code"
+        )"
+        OAUTH2_ACCESS_TOKEN="$(echo "$OAUTH2_RESPONSE_JSON" | jq -r .access_token)"
+        error="$(echo "$OAUTH2_RESPONSE_JSON" | jq -r .error)"
+        if [ "$OAUTH2_ACCESS_TOKEN" != "null" ]; then
+            break
+        fi
+        if [ "$error" != "authorization_pending" ]; then
+	        echo "Error response from keycloak: $(printf "%q" "$error")" 1>&2
+            echo "$OAUTH2_RESPONSE_JSON" | jq . 1>&2
+	        return 1
+	    fi
+    done
+}
+
+login() {
+    local mode="${1:-$DEFAULT_LOGIN_MODE}"
+    case "$mode" in
+        client-credentials)
+            login_via_client_credentials || return $?
+            ;;
+        device)
+            login_via_device || return $?
+            ;;
+        *)
+            echo "Error: Unknown login mode $mode!" 1>&2
+            return 1
+    esac
+
+    if $DEBUG_TOKEN ; then
+        echo "$OAUTH2_ACCESS_TOKEN" | jwtutil --decode --header --pp 1>&2
+        echo "$OAUTH2_ACCESS_TOKEN" | jwtutil --decode --pp 1>&2
+    fi
+
+    case "$OUTPUT_FORMAT" in
+        raw) echo "$OAUTH2_ACCESS_TOKEN" ;;
+        bash) echo "export OAUTH2_ACCESS_TOKEN=$(printf "%q" "$OAUTH2_ACCESS_TOKEN")" ;;
+        json) echo "$OAUTH2_RESPONSE_JSON" ;;
+        *) echo "Error: Unknown output format: $OUTPUT_FORMAT" 1>&2; return 1 ;;
+    esac
+}
+
 
 ################################################################################
 # Main, argparsing and commands
@@ -250,122 +380,8 @@ cmd_check-token() {
     fi
 }
 
-cmd_login-client-credentials() {
-    declare response_json access_token
-    if [[ -z "$OAUTH2_CLIENT_SECRET" ]] ; then
-        echo "Error: OAUTH2_CLIENT_SECRET is not set" 1>&2
-        return 1
-    fi
-
-    configure || return $?
-
-    # FIXME: Handle HTTP errors
-    response_json="$(
-        invoke curl -s -X POST \
-            --url "${OAUTH2_TOKEN_ENDPOINT}" \
-            --header 'Content-Type: application/x-www-form-urlencoded' \
-            --data 'grant_type=client_credentials' \
-            --data "client_id=$OAUTH2_CLIENT_ID" \
-            --data "client_secret=$OAUTH2_CLIENT_SECRET" \
-            --data "audience=$OAUTH2_AUDIENCE"
-    )"
-
-    access_token="$(echo "$response_json" | jq -r '.access_token')"
-    if $DEBUG_TOKEN ; then
-        echo "$access_token" | jwtutil --decode --header --pp 1>&2
-        echo "$access_token" | jwtutil --decode --pp 1>&2
-    fi
-    case "$OUTPUT_FORMAT" in
-        raw) echo "$access_token" ;;
-        bash) echo "export OAUTH2_ACCESS_TOKEN=$(printf "%q" "$access_token")" ;;
-        json) echo "$response_json" ;;
-        *) echo "Error: Unknown output format: $OUTPUT_FORMAT" 1>&2; return 1 ;;
-    esac
-}
-
-cmd_login-device() {
-    # See https://git.mobilexag.de/prd-serviceplatform/documentation/-/blob/develop/modules/operations-guide/examples/read-token-from-keycloak.sh?ref_type=heads
-    # https://datatracker.ietf.org/doc/html/rfc8628#section-3.1
-    # TODO: Not tested yet!
-    local response_json device_code user_code verification_uri expires interval expire_seconds error access_token
-    local -a request_data
-
-    configure || return $?
-
-    request_data=(
-        --data "client_id=$OAUTH2_CLIENT_ID"
-        --data "scope=$OAUTH2_SCOPE"
-    )
-
-    if [ -n "$OAUTH2_CLIENT_SECRET" ] ; then
-        request_data+=( --data "client_secret=$OAUTH2_CLIENT_SECRET" )
-    fi
-    
-    response_json="$(
-        # Note: Client authentication is enabled in keycloak, so we need to pass the client_secret
-        # This is not neccessary for other clients like crossmip, where the client is public
-        invoke curl -s -X POST \
-            --url "$OAUTH2_DEVICE_AUTHORIZATION_ENDPOINT" \
-            --header 'content-type: application/x-www-form-urlencoded' \
-            ${request_data[@]}
-    )"
-    echo "response_json: $response_json" 1>&2
-
-    error="$(echo "$response_json" | jq -r .error)"
-    if [ "$error" != "null" ]; then
-        echo "Error response from keycloak: $(printf "%q" "$error")" 1>&2
-        echo "$response_json" | jq . 1>&2
-        return 1
-    fi
-
-    device_code="$(echo "$response_json" | jq -r .device_code)"
-    user_code="$(echo "$response_json" | jq -r .user_code)"
-    verification_uri="$(echo "$response_json" | jq -r .verification_uri)"
-    expires="$(echo "$response_json" | jq -r .expires_in)"
-    interval="$(echo "$response_json" | jq -r .interval)"
-
-    (
-        echo "Please:"
-        echo "    * open $verification_uri"
-        echo "    * enter $user_code and log in"
-        open "$verification_uri"
-    ) 1>&2
-
-    # Loop until the token is available or the timeout is reached
-    expire_seconds=$((SECONDS+expires))
-    while [ $SECONDS -lt $expire_seconds ]; do
-        sleep "$interval"
-        response_json="$(
-            invoke curl -s -X POST \
-                --url "$OAUTH2_TOKEN_ENDPOINT" \
-                --header 'content-type: application/x-www-form-urlencoded' \
-                --data "client_id=$OAUTH2_CLIENT_ID" \
-                --data "client_secret=$OAUTH2_CLIENT_SECRET" \
-                --data "device_code=$device_code" \
-                --data "grant_type=urn:ietf:params:oauth:grant-type:device_code"
-        )"
-        access_token="$(echo "$response_json" | jq -r .access_token)"
-        error="$(echo "$response_json" | jq -r .error)"
-        if [ "$access_token" != "null" ]; then
-            break
-        fi
-        if [ "$error" != "authorization_pending" ]; then
-	        echo "Error response from keycloak: $(printf "%q" "$error")" 1>&2
-            echo "$response_json" | jq . 1>&2
-	        return 1
-	    fi
-    done
-    
-    if $DEBUG_TOKEN ; then
-        echo "$access_token" | jwtutil --decode --header --pp 1>&2
-        echo "$access_token" | jwtutil --decode --pp 1>&2
-    fi
-    case "$OUTPUT_FORMAT" in
-        raw) echo "$access_token" ;;
-        bash) echo "export OAUTH2_ACCESS_TOKEN=$(printf "%q" "$access_token")" ;;
-        json) echo "$response_json" ;;
-        *) echo "Error: Unknown output format: $OUTPUT_FORMAT" 1>&2; return 1 ;;
-    esac
+cmd_login() {
+    login "$@"
 }
 
 cmd_read-token() {
@@ -379,92 +395,6 @@ cmd_read-token() {
 cmd_curl() {
     # FIXME: Check if OAUTH2_ACCESS_TOKEN is set or expired
     invoke -l 1 curl -H "Authorization: Bearer $OAUTH2_ACCESS_TOKEN" "$@"
-}
-
-_graphql_query() {
-    cat <<'EOF'
-query installationsByAddressStreetFragment {
-  installationsByFragments(nameFragment: "bay", zipFragment: "81", streetFragment: "Schön") {
-    id,
-    displayIdJson{jsonString},
-    shortTextJson{jsonString},
-    address {id,streetJson{jsonString},streetNumberJson{jsonString},zipJson{jsonString},cityJson{jsonString}},
-    contact {
-      id,
-      firstNameJson{jsonString},
-      lastNameJson{jsonString},
-      contactComms{
-        type,
-        position,
-        valueJson{jsonString},
-      }
-    },
-  }
-}
-EOF
-}
-
-_graphql_query2() {
-    cat <<'EOF'
-query contactAddressByFragments {
-  contactAddressByFragments(nameFragment: "dummy", zipFragment: "81543", streetFragment: "Schön") {
-    id, externalId,
-    address {id,externalId,streetJson{jsonString},streetNumberJson{jsonString},zipJson{jsonString},cityJson{jsonString}},
-    contact {
-      id, externalId,
-      firstNameJson{jsonString},
-      lastNameJson{jsonString},
-      contactComms{
-        type,
-        position,
-        valueJson{jsonString},
-      },
-      installations {
-        id, externalId,
-        shortTextJson{jsonString},
-      }
-    },
-  }
-}
-EOF
-}
-
-_graphql_request() {
-    _graphql_query | jq --raw-input --slurp '{"query":.}'
-}
-
-# Example request to a crossmip rest api (online-search-service)
-cmd_options-request() {
-    #declare target_url="${1:-"https://online-search.kyma-dev.mobilex-serviceplatform.com/graphql"}"
-    declare target_url="${1:-"https://customer-687399110-mip.kyma-dev.mobilex-serviceplatform.com/sync-api/3.0/tenants/1/online-search/graphql"}"
-    _graphql_request | invoke -l 1 curl -v -X OPTIONS "$target_url" \
-        -H "Origin: https://some-custom-origin.com" \
-        -H "Authorization: Bearer $OAUTH2_ACCESS_TOKEN" \
-        -H "Content-Type: application/json" \
-        --data-binary @-
-}
-
-# Example request to a crossmip rest api (online-search-service)
-cmd_example-request() {
-    #declare target_url="${1:-"https://online-search.kyma-dev.mobilex-serviceplatform.com/graphql"}"
-    declare target_url="${1:-"https://customer-687399110-mip.kyma-dev.mobilex-serviceplatform.com/sync-api/3.0/tenants/1/online-search/graphql"}"
-    _graphql_request | invoke -l 1 curl -v -X POST "$target_url" \
-        -H "Origin: https://6546848" \
-        -H "Authorization: Bearer $OAUTH2_ACCESS_TOKEN" \
-        -H "Content-Type: application/json" \
-        --data-binary @-
-}
-
-cmd_get-graphql-schema() {
-    #declare target_url="${1:-"https://online-search.kyma-dev.mobilex-serviceplatform.com/graphql/schema.graphql"}"
-    declare target_url="${1:-"https://customer-687399110-mip.kyma-dev.mobilex-serviceplatform.com/sync-api/3.0/tenants/1/online-search/graphql/schema.graphql"}"
-    invoke -l 1 curl -v -X GET "$target_url" \
-        -H "Origin: https://some-custom-origin.com" \
-        -H "Authorization: Bearer $OAUTH2_ACCESS_TOKEN"
-}
-
-cmd_xx() {
-    _graphql_request
 }
 
 cmd_help() {
