2280a05ff242889c86dc26cbd3b9a18f75d47502
Markus Dangl <663399+xicesky@users.noreply.github.com>
2025-12-11 05:37:04 +0100

    Add pipenv to environment.

--- environment/python.sh
+++ environment/python.sh
@@ -8,3 +8,9 @@ if [[ -d ~/Library/Python ]] ; then
         $FIND ~/Library/Python -mindepth 1 -maxdepth 1 -type d -printf "%f\0" | $SORT -z --version-sort -r
     )
 fi
+
+# Pipenv install according to https://pipenv.pypa.io/en/latest/installation.html
+if [[ -d "$HOME/.pipenv-venv/bin" ]] ; then
+    export_variable PIPENV_IGNORE_VIRTUALENVS 1
+    append_to_path "$HOME/.pipenv-venv/bin"
+fi
