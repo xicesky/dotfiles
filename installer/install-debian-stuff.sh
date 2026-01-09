2b1cc0521856abe9f27e1866f253de598b8d5601
Markus Dangl <663399+xicesky@users.noreply.github.com>
2026-01-09 15:14:10 +0100

    installer: Add general debian script for RPi

---
+++ installer/install-debian-stuff.sh
@@ -0,0 +1,304 @@
+#!/bin/bash
+
+################################################################################
+# Verbosity, command logging
+
+# Verbosity level, 0 is quiet, 1 is normal, 2 prints commands
+VERBOSITY="${VERBOSITY:-2}"
+
+# Don't act, just check (and print commands)
+NO_ACT="${NO_ACT:-false}"
+
+# Display a message if verbosity level allows
+log() {
+    declare prefix="$DEFAULT_LOG_PREFIX"
+    [[ "$1" = "-p" ]] && { shift; prefix="$1"; shift; }
+    declare level="$1"; shift
+    if [[ "$VERBOSITY" -ge "$level" ]] ; then
+        echo "$prefix$*" 1>&2
+    fi
+}
+
+# Same but use printf
+logf() {
+    declare level="$1"; shift
+    # Passing a formatting string is exactly what we intend here
+    # shellcheck disable=SC2059
+    log "$level" "$(printf "$@")"
+}
+
+# Stores the last command executed by invoke()
+declare -a LAST_COMMAND
+LAST_COMMAND=()
+
+# ... and it's exit code
+declare -g LAST_COMMAND_EXITCODE
+LAST_COMMAND_EXITCODE=0
+
+# Log and invoke a command or skip it if NO_ACT is set
+# This actually works only for simple commands, you can't use it to:
+#   - Invoke commands that use pipes or redirections
+#   - Invoke compound commands (expressions)
+#   - Set variables
+invoke() {
+    declare redir=""
+    while [[ $# -gt 0 ]] ; do
+        case "$1" in
+            "-o")   shift; redir="$1"; shift ;;
+            -*)     echo "invoke: Unknown flag: $arg" 1>&2; return 1 ;;
+            *)      break ;;
+        esac
+    done
+    LAST_COMMAND=("$@")
+    log -p "" 2 "$(printf "%q " "$@")"
+    if ! "$NO_ACT" ; then
+        LAST_COMMAND_EXITCODE=0
+        if [[ -z "$redir" ]] ; then "$@"; else "$@" >"$redir"; fi
+        LAST_COMMAND_EXITCODE="$?"
+        return $LAST_COMMAND_EXITCODE
+    fi
+}
+
+# Report the last command, if it failed
+report_command_failure() {
+    if [[ "$LAST_COMMAND_EXITCODE" -ne 0 ]] ; then
+        log 1 "Last command executed:"
+        log 1 "    $(printf "%q " "${LAST_COMMAND[@]}")"
+        log 1 "Returned exit code ${LAST_COMMAND_EXITCODE}"
+    fi
+}
+
+################################################################################
+# Temp dir handling
+
+# Stores the name of the temporary directory, if it was created
+declare -g SCRIPT_TEMP_DIR="${SCRIPT_TEMP_DIR:-}"
+declare -g THIS_SCRIPT_NAME
+THIS_SCRIPT_NAME="$(basename "$0")"
+
+# Create temporary directory (once) and return its name
+# This still works when NO_ACT is set, because the effect is only temporary
+require-temp-dir() {
+    if [[ -z "$SCRIPT_TEMP_DIR" ]] ; then
+        SCRIPT_TEMP_DIR="$(mktemp -d --tmpdir "$THIS_SCRIPT_NAME".XXXXXXXXXX)" \
+            || echo "Error: Failed to create temporary directory." 1>&2 \
+            || return $?
+        # Remove temporary directory when this script finishes
+        trap 'remove-temp-dir' EXIT
+    fi
+}
+
+# Remove temporary directory if it was ever created
+remove-temp-dir() {
+    if [[ -n "$SCRIPT_TEMP_DIR" ]] ; then
+        log 2 "# Removing temporary directory: $SCRIPT_TEMP_DIR"
+        rm -rf "$SCRIPT_TEMP_DIR" \
+            || echo "Error: Failed to remove temporary directory (exitcode $?): $SCRIPT_TEMP_DIR" 1>&2
+        SCRIPT_TEMP_DIR=''
+    fi
+}
+
+################################################################################
+# Utilities
+
+# Search for the given executable in PATH
+# avoids a dependency on the `which` command
+which() {
+    # Alias to Bash built-in command `type -P`
+    type -P "$@"
+}
+
+# Put (multiple) relative symlink(s) in a target directory
+# Skips without error if file exists
+# Sources are relative to the target directory, not the current directory!
+lins() {
+    declare -a sources=()
+    while [[ $# -gt 1 ]] ; do
+        sources+=( "$1" ); shift
+    done
+    declare target="$1"; shift
+    if [[ ! -d "$target" ]] ; then
+        echo "Error: Target must be a directory, but is: $target" 1>&2
+        return 1
+    fi
+    declare i
+    declare bn
+    for i in "${sources[@]}" ; do
+        bn="$(basename "$i")"
+        # FIXME: Check that source exists
+        if [[ -e "$target/$bn" ]] ; then
+            echo "$target/$bn already exists." 1>&2
+            continue
+        fi
+        invoke ln -s "$i" "$target/$bn" || return 1
+    done
+}
+
+################################################################################
+# Configuration
+
+DOTFILES_DIR="${DOTFILES_DIR:-$HOME/_dotfiles}"
+# Everything else is configured via _dotfiles/installer/install.sh
+
+configuration_load() {
+    if [[ -x "$DOTFILES_DIR/installer/install.sh" ]] ; then
+        eval "$("$DOTFILES_DIR/installer/install.sh" -q info)"
+    else
+        log 0 "Dotfiles main installer $DOTFILES_DIR/installer/install.sh not found or not executable"
+        return 1
+    fi
+}
+
+################################################################################
+# Command parts / installation steps
+
+install_prereqs() {
+    invoke sudo apt-get update || return 1
+    invoke sudo apt-get dist-upgrade || return 1
+    invoke sudo apt-get install -y git etckeeper python3-pip || return 1
+}
+
+install_tools_debian() {
+    invoke sudo apt-get install -y \
+        vim p7zip-full lzma pigz gzrt gzip bzip2 \
+        mc curl wget nmap hwinfo strace htop iotop iptraf-ng tcpdump \
+        ncftp netcat-openbsd pv dos2unix tmux \
+        bc zsh dnsutils git gnupg2 \
+        ca-certificates jq shellcheck xmlstarlet golang \
+        aptitude asciidoctor ruby-rouge postgresql-client \
+        || return 1
+}
+
+install_homebrew() {
+    if [[ "$(uname -m)" != "x86_64" ]] ; then
+        return 0
+    fi
+
+    # Install homebrew
+    if [[ -e /home/linuxbrew ]] ; then
+        echo "/home/linuxbrew already exists - not installing homebrew"
+        return 0
+    fi
+    if [[ ! -d ~/_dotfiles ]] ; then
+        echo "Dotfiles are not in ~/_dotfiles (yet?)" 1>&2
+        return 1
+    fi
+    if [[ ! -f ~/_dotfiles/homebrew/install.sh ]] ; then
+        #shellcheck disable=SC2088
+        echo "~/_dotfiles/homebrew/install.sh is missing" 1>&2
+        return 1
+    fi
+
+    echo "################################################################################"
+    echo "# Starting homebrew install"
+    echo ""
+    /bin/bash ~/_dotfiles/homebrew/install.sh || return 1
+    if [[ ! -x /home/linuxbrew/.linuxbrew/bin/brew ]] ; then
+        echo "Homebrew did not install /home/linuxbrew/.linuxbrew/bin/brew ???" 1>&2
+        return 1
+    fi
+    echo "# Homebrew install done"
+}
+
+install_tools_homebrew() {
+    if [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]] ; then
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" || return 1
+        invoke "$HOMEBREW_PREFIX"/bin/brew install \
+            yq shfmt gcc helm terraform hadolint kubectl ffmpeg pipx
+    fi
+}
+
+install_tools_pipx() {
+    pipx install yt-dlp
+}
+
+change_shell() {
+    echo ""
+    echo "Changing shell to /usr/bin/zsh -- please enter your password:"
+    invoke chsh -s /usr/bin/zsh || return 1
+}
+
+################################################################################
+# Main, argparsing and commands
+
+cmd_install() {
+    invoke install_prereqs || return 1
+    configuration_load || return 1
+    invoke "$DOTFILES_DIR/installer/install.sh" install-only
+    invoke install_tools_debian || return 1
+    invoke install_homebrew || return 1
+    invoke install_tools_homebrew || return 1
+    invoke install_tools_pipx || return 1
+}
+
+cmd_install-tools() {
+    configuration_load || return 1
+    invoke "$DOTFILES_DIR/installer/install.sh" install
+    invoke install_tools_debian || return 1
+    invoke install_homebrew || return 1
+    invoke install_tools_homebrew || return 1
+    invoke install_tools_pipx || return 1
+}
+
+cmd_install_dotfiles() {
+    configuration_load || return 1
+    invoke "$DOTFILES_DIR/installer/install.sh" install
+}
+
+cmd_help() {
+    usage
+}
+
+usage() {
+    echo "Usage: $0 [global flags...] <command...>"
+    echo "Global flags:"
+    echo "    -v    Increase verbosity level"
+    echo "    -q    Decrease verbosity level"
+    echo "    --help  Show usage and exit"
+    echo ""
+    echo "Available commands:"
+    echo "    install"
+    echo "    install-tools"
+    echo "    help    Show usage and exit"
+    echo ""
+}
+
+main() {
+    declare cmd=""
+    declare cmderr=0
+    while [[ $# -gt 0 ]] ; do
+        arg="$1"; shift
+        case "$arg" in
+            -v) (( VERBOSITY++ )) ;;
+            -q) (( VERBOSITY-- )) ;;
+            --help)
+                cmd=help
+                break
+                ;;
+            -*)
+                { echo "Unknown flag: $arg"; usage; } 1>&2
+                return 1
+                ;;
+            *)
+                    cmd="$arg"
+                break
+                ;;
+        esac
+    done
+    if [[ -z "$cmd" ]] ; then
+        usage
+    elif [[ $(type -t "cmd_$cmd") == function ]] ; then
+        "cmd_$cmd" "$@"
+        cmderr="$?"
+        if [[ "$cmderr" -ne 0 && "$LAST_COMMAND_EXITCODE" -ne 0 ]] ; then
+            report_command_failure 1>&2
+            echo "" 1>&2
+        fi
+        return "$cmderr"
+    else
+        { echo "Unknown command: $cmd"; usage; } 1>&2
+        return 1
+    fi
+}
+
+main "$@"
