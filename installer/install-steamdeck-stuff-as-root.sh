2c5e9038de25550234d4c0743e8eb38ceb00226d
Markus Dangl <663399+xicesky@users.noreply.github.com>
2026-02-19 21:44:14 +0100

    bin/installer: install x11vnc on steamdeck

--- installer/install-steamdeck-stuff-as-root.sh
+++ installer/install-steamdeck-stuff-as-root.sh
@@ -28,5 +28,5 @@ steamos-readonly disable
 pacman-key --init
 pacman-key --populate archlinux
 pacman-key --populate holo
-pacman -Sy openbsd-netcat pv dog mc python-pip python-pipx
+pacman -Sy openbsd-netcat pv dog mc python-pip python-pipx x11vnc
 steamos-readonly enable
